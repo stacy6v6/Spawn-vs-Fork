d82327f9e9e785d653bcea2d048214471f6e359f
Stacy Marie Pelito <stacymarie.pelito@stacymarie.pelito-C52FJWWCQJ>
2023-01-11 07:13:47 +0800

    fixed write to child placement and variable names

--- fparent.js
+++ fparent.js
@@ -1,3 +1,4 @@
+import { freemem } from 'node:os'
 import { readFileSync } from 'node:fs'
 import { fork } from 'node:child_process'
 import * as dotenv from 'dotenv'
@@ -5,37 +6,38 @@ import {
   decrypt, keyPairGen, sign, verify,
 } from './helper.js'
 
+const startMem = freemem()
 dotenv.config()
 
 try {
   const secret = process.env.SECRET
   const algo = 'SHA256'
   const { publicKey, privateKey } = keyPairGen(secret)
-  const fname = './plaintext.txt'
-  const content = readFileSync(fname)
-  const sig = sign(algo, content, privateKey, secret)
+  const filePath = './plaintext.txt'
+  const content = readFileSync(filePath)
+  const signature = sign(algo, content, privateKey, secret)
   const child = fork('./fchild.js')
   const sendObj = {
     pubKey: publicKey,
-    file: fname,
+    file: filePath,
   }
 
-  child.send(JSON.stringify(sendObj))
-
   child.on('message', (message) => {
-    const buf = Buffer.from(message.toString(), 'base64')
+    const childFile = readFileSync(message)
+    const buf = Buffer.from(childFile.toString(), 'base64')
     const decrypted = decrypt(privateKey, secret, buf)
-    const verified = verify(algo, decrypted, publicKey, sig) ? decrypted.toString() : 'Invalid Message'
+    const decryptedMessage = verify(algo, decrypted, publicKey, signature) ? decrypted.toString() : 'Invalid Message'
     console.table([{
       time: process.uptime(),
-      message: verified,
-      memoryConsumed: process.memoryUsage().external,
+      message: decryptedMessage,
+      memoryConsumed: (startMem - freemem()) / 1048576,
     }])
     process.exit(0)
   })
   process.on('error', (error) => {
     console.log(error)
   })
+  child.send(JSON.stringify(sendObj))
 } catch (err) {
   console.log(err.message)
 }
--- parent.js
+++ parent.js
@@ -1,3 +1,4 @@
+import { freemem } from 'node:os'
 import { readFileSync } from 'node:fs'
 import { spawn } from 'node:child_process'
 import * as dotenv from 'dotenv'
@@ -5,31 +6,31 @@ import {
   decrypt, keyPairGen, sign, verify,
 } from './helper.js'
 
+const startMem = freemem()
 dotenv.config()
 
 try {
   const secret = process.env.SECRET
   const algo = 'SHA256'
   const { publicKey, privateKey } = keyPairGen(secret)
-  const fname = './plaintext.txt'
-  const content = readFileSync(fname)
-  const sig = sign(algo, content, privateKey, secret)
+  const filePath = './plaintext.txt'
+  const content = readFileSync(filePath)
+  const signature = sign(algo, content, privateKey, secret)
   const child = spawn('node', ['./child.js'])
   const sendObj = {
     pubKey: publicKey,
-    file: fname,
+    file: filePath,
   }
 
-  child.stdin.write(JSON.stringify(sendObj))
-
   child.stdout.on('data', (data) => {
-    const buf = Buffer.from(data.toString(), 'base64')
+    const childFile = readFileSync(data)
+    const buf = Buffer.from(childFile.toString(), 'base64')
     const decrypted = decrypt(privateKey, secret, buf)
-    const verified = verify(algo, decrypted, publicKey, sig) ? decrypted.toString() : 'Invalid Message'
+    const decryptedMessage = verify(algo, decrypted, publicKey, signature) ? decrypted.toString() : 'Invalid Message'
     console.table([{
       time: process.uptime(),
-      message: verified,
-      memoryConsumed: process.memoryUsage().external,
+      message: decryptedMessage,
+      memoryConsumed: (startMem - freemem()) / 1048576,
     }])
     process.exit(0)
   })
@@ -37,6 +38,7 @@ try {
   child.stderr.on('data', (err) => {
     console.log(err.message)
   })
+  child.stdin.write(JSON.stringify(sendObj))
 } catch (err) {
   console.log(err.message)
 }
